ea41b1cbafb86e38e8f3e218e4c41b668c84b858
leonardoman9 <46076388+leonardoman9@users.noreply.github.com>
2025-02-05 11:15:23 +0100

    cesso

--- openai_request.py
+++ openai_request.py
@@ -130,7 +130,7 @@ def detect_wake_word():
     porcupine = pvporcupine.create(
         access_key=porcupine_access_key,
         keywords=["Cesso"],
-        model_path = "porcupine_params_it.pv"
+        model_path = "porcupine_params_it.pv",
         keyword_paths=["Cesso_it_raspberry-pi_v3_0_0.ppn"]
     )
     pa = pyaudio.PyAudio()
